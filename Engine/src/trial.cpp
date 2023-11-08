abda4d7b2b7a6bb0b9090b47d1a4de484ee35801
DragonzHyper Man <eurekaplayz@gmail.com>
2023-11-08 17:40:31 +0530

    Initial Commit

---
+++ Engine/src/trial.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+#include "trial.hpp"
+
+int add_smth(int i, int j) { return i+j; }
---
+++ Sandbox/src/Main.cpp
@@ -0,0 +1,8 @@
+#include "../../Engine/include/Engine.hpp" 
+
+#include <iostream>
+
+int main(int argc, const char **argv) {
+	std::cout << add_smth(1, 2);
+	return (0);
+}
